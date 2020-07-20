195025979c0d4b3a8047d9f50d41b08c421b5022
jose-marquez89 <jlmarquez89@gmail.com>
2020-07-20 17:36:22 -0500

    completes the run function and other changes

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,6 +2,7 @@
 
 import sys
 
+
 class CPU:
     """Main CPU class."""
 
@@ -10,7 +11,16 @@ class CPU:
         self.ram = [0] * 256
         self.registers = [0] * 8
         self.pc = 0
+        self.ir = 0
+        self.fl = 0
+        self.interpreter = {"HLT": 0b00000001, "LDI": 0b10000010,
+                            "PRN": 0b01000111}
 
+    def ram_read(self, mar):
+        return self.ram[mar]
+
+    def ram_write(self, mar, mdr):
+        self.ram[mar] = mdr
 
     def load(self):
         """Load a program into memory."""
@@ -21,25 +31,24 @@ class CPU:
 
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
+            0b10000010,  # LDI R0,8
             0b00000000,
             0b00001000,
-            0b01000111, # PRN R0
+            0b01000111,  # PRN R0
             0b00000000,
-            0b00000001, # HLT
+            0b00000001,  # HLT
         ]
 
         for instruction in program:
             self.ram[address] = instruction
             address += 1
 
-
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -51,8 +60,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -65,4 +74,26 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        self.load()
+
+        running = True
+
+        while running:
+            self.ir = self.ram_read(self.pc)
+            operand_a = self.pc + 1
+            operand_b = self.pc + 2
+
+            if self.ir == self.interpreter["HLT"]:
+                running = False
+                self.pc += 1
+                break
+            elif self.ir == self.interpreter["PRN"]:
+                print(self.registers[self.ram_read(operand_a)])
+                self.pc += 2
+            elif self.ir == self.interpreter["LDI"]:
+                self.registers[self.ram[operand_a]] = self.ram[operand_b]
+                self.pc += 3
+            else:
+                running = self.alu(self.ir, operand_a, operand_b)
+
+        sys.exit()
