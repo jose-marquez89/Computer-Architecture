63768271f2310e999e435f64acdc03a10d3f8b07
jose-marquez89 <jose_marquez89@outlook.com>
2020-07-24 10:10:29 -0500

    fixes bugs in timer interrupt

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -20,10 +20,12 @@ class CPU:
         self.ir = 0
         self.fl = 0
 
+        # for future development, consider creating
+        # separate class attribute variables for 
+        # each register for easier naming. Consider
+        # trade-off as well, will require a hash table
+
         self.registers[7] = 0xF4
-        self._is = self.registers[6]
-        self._im = self.registers[5]
-        self.sp = self.registers[7]
 
         self.i_vectors = {0: 0xF8,
                           1: 0xF9,
@@ -50,7 +52,7 @@ class CPU:
             prog = f.readlines()
             logging.debug(f"File Split: {prog}")
             for line in prog:
-                if line.startswith("#"):
+                if line.startswith("#") or line.startswith("\n"):
                     continue
                 self.ram_write(valid_for_address, int(line[:8], 2))
                 valid_for_address += 1
@@ -69,6 +71,12 @@ class CPU:
         print(self.registers[self.ram_read(operand)])
         return True
 
+    def pra(self):
+        """Prints ASCII character in the specified register"""
+        reg = self.ram_read(self.pc + 1)
+        print(chr(self.registers[reg]))
+        return True
+
     def ldi(self):
         operand_a = self.pc + 1
         operand_b = self.pc + 2
@@ -76,8 +84,8 @@ class CPU:
         return True
 
     def jmp(self):
-        reg = self.pc + 1
-        self.pc = self.registers[self.ram_read(reg)]
+        reg = self.ram_read(self.pc + 1)
+        self.pc = self.registers[reg]
         return -1
 
     def st(self):
@@ -107,46 +115,46 @@ class CPU:
 
     def push(self, from_call=False, from_interrupt=False):
         """Push the content of the specified register on to the stack"""
-        self.sp -= 1
+        self.registers[7] -= 1
 
         # Get the instruction directly after call, if subroutine
         if from_call:
-            self.ram_write(self.sp, self.pc + 2)
+            self.ram_write(self.registers[7], self.pc + 2)
             return True
         elif from_interrupt:
             # push pc and fl to stack
-            self.ram_write(self.sp, self.pc)
-            self.sp -= 1
-            self.ram_write(self.sp, self.fl)
+            self.ram_write(self.registers[7], self.pc)
+            self.registers[7] -= 1
+            self.ram_write(self.registers[7], self.fl)
             for i in range(0, 7):
-                self.sp -= 1
-                self.ram_write(self.sp, self.registers[i])
+                self.registers[7] -= 1
+                self.ram_write(self.registers[7], self.registers[i])
 
             return True
 
-        logging.debug(f"Pushing to stack at address {hex(self.sp)}")
+        logging.debug(f"Pushing to stack at address {hex(self.registers[7])}")
         copy_reg = self.ram_read(self.pc + 1)
         logging.debug(f"copy_reg (PUSH): {copy_reg}")
 
-        self.ram_write(self.sp, self.registers[copy_reg])
+        self.ram_write(self.registers[7], self.registers[copy_reg])
         return True
 
     def pop(self, from_ret=False):
-        logging.debug(f"Popping from stack at address {hex(self.sp)}")
-        if self.sp == 0xF4:
+        logging.debug(f"Popping from stack at address {hex(self.registers[7])}")
+        if self.registers[7] == 0xF4:
             print("Program attempted to pop from an empty stack. Aborting.")
             return False
 
         # if returning from subroutine, set pc
         if from_ret:
-            self.pc = self.ram_read(self.sp)
-            self.sp += 1
+            self.pc = self.ram_read(self.registers[7])
+            self.registers[7] += 1
             return True
 
         copy_reg = self.ram_read(self.pc + 1)
 
-        self.registers[copy_reg] = self.ram_read(self.sp)
-        self.sp += 1
+        self.registers[copy_reg] = self.ram_read(self.registers[7])
+        self.registers[7] += 1
         return True
 
     def call(self):
@@ -163,14 +171,14 @@ class CPU:
     def iret(self):
         # pop registers 6-0 from stack
         for i in range(6, -1, -1):
-            self.registers[i] = self.ram_read(self.sp)
-            self.sp += 1
+            self.registers[i] = self.ram_read(self.registers[7])
+            self.registers[7] += 1
         # pop flags
-        self.fl = self.ram_read(self.sp)
-        self.sp += 1
+        self.fl = self.ram_read(self.registers[7])
+        self.registers[7] += 1
         # pop program counter
-        self.pc = self.ram_read(self.sp)
-        self.sp += 1
+        self.pc = self.ram_read(self.registers[7])
+        self.registers[7] += 1
         # re-enable interrupts
         self.allow_interrupts = True
 
@@ -186,7 +194,8 @@ class CPU:
                             0b00010001: self.ret,
                             0b00010011: self.iret,
                             0b01010100: self.jmp,
-                            0b10000100: self.st}
+                            0b10000100: self.st,
+                            0b01001000: self.pra}
         return
 
     def build_alu_ops(self):
@@ -234,25 +243,29 @@ class CPU:
         logging.debug(f"Start pc: {self.pc}")
 
         now = time.time()
+     
         while running:
             # check the timer to see if 1 second has passed
             elapsed = time.time() - now
 
+
             # if interrupts are enabled, set zero-th bit for timer
             if self.allow_interrupts and elapsed >= 1.0:
-                self._is = self.set_kth_bit(0, self._is)
+                self.registers[6] = self.set_kth_bit(0, self.registers[6])
+                now = time.time()
 
             if self.allow_interrupts:
                 # check for interupts
-                masked_interrupts = self._im & self._is
+                masked_interrupts = self.registers[5] & self.registers[6]
 
                 for i in range(8):
                     interrupt = ((masked_interrupts >> i) & 1) == 1
                     if interrupt:
+                        logging.debug("Found interrupt, disabling interruptions")
                         self.allow_interrupts = False
 
                         # unset bit
-                        self._is = self._is ^ (1 << i)
+                        self.registers[6] = self.registers[6] ^ (1 << i)
 
                         # push items onto stack (push automatically)
                         self.push(from_interrupt=True)
